ed362f4c4c46337bb570098fa1f493419828ec63
qhxiv <eknight1412@gmail.com>
2024-02-21 16:16:55 +0700

    add animation for song list

---
+++ assets/javascript/main.js
@@ -0,0 +1,27 @@
+function handleListAnimation() {
+  const listBtn = document.querySelector('.buttons__list');
+  const list = document.querySelector('.list');
+  const mainList = document.querySelector('.list__main');
+  const overlay = document.querySelector('.list__overlay');
+  const closeBtn = document.querySelector('.list__header i');
+
+  listBtn.onclick = () => {
+    list.style.display = 'flex';
+  };
+
+  closeBtn.onclick = () => {
+    mainList.style.animation = 'slideOut 0.5s ease forwards';
+    overlay.style.animation = 'fadeOut 0.5s ease forwards';
+    setTimeout(() => {
+      list.style.display = 'none';
+      mainList.style.animation = 'slideIn 0.5s ease';
+      overlay.style.animation = 'fadeIn 0.5s ease';
+    }, 500);
+  };
+}
+
+function start() {
+  handleListAnimation();
+}
+
+start();
