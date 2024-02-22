d2077f40a5f4296e2799ba2b960532a0ac1bcb80
qhxiv <eknight1412@gmail.com>
2024-02-22 13:29:21 +0700

    add volume button animation

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,5 +1,5 @@
 function handleListAnimation() {
-  const listBtn = document.querySelector('.buttons__list');
+  const listBtn = document.querySelector('.menu');
   const list = document.querySelector('.list');
   const mainList = document.querySelector('.list__main');
   const overlay = document.querySelector('.list__overlay');
@@ -12,6 +12,7 @@ function handleListAnimation() {
   closeBtn.onclick = () => {
     mainList.style.animation = 'slideOut 0.5s ease forwards';
     overlay.style.animation = 'fadeOut 0.5s ease forwards';
+
     setTimeout(() => {
       list.style.display = 'none';
       mainList.style.animation = 'slideIn 0.5s ease';
@@ -20,8 +21,28 @@ function handleListAnimation() {
   };
 }
 
+function handleVolumeButton() {
+  const volBtn = document.querySelector('.volume');
+  console.log(volBtn);
+  const volWrapper = document.querySelector('.volume__wrapper');
+  const volPanel = document.querySelector('.volume__panel');
+
+  volBtn.addEventListener('mouseover', (e) => {
+    volWrapper.style.display = 'flex';
+    volPanel.style.animation = 'slideIn 0.2s ease';
+  });
+
+  volBtn.addEventListener('mouseout', (e) => {
+    volPanel.style.animation = 'slideOut 0.2s ease forwards';
+    setTimeout(() => {
+      volWrapper.style.display = 'none';
+    }, 200);
+  });
+}
+
 function start() {
   handleListAnimation();
+  handleVolumeButton();
 }
 
 start();
