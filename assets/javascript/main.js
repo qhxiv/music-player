9f3d94d729dbfae5abca9d741b1b78e4d90c9720
qhxiv <eknight1412@gmail.com>
2024-02-26 13:04:36 +0700

    show each song duration on the playlist; add functionality to the play button

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,7 +1,19 @@
+import songs from './songs.js';
+
+const listContent = document.querySelector('.list__content');
+const audio = document.querySelector('audio');
+const currentTime = document.querySelector('.progress__current');
+const duration = document.querySelector('.progress__total');
+
+let currentIndex = 0;
+let oldIndex = 0;
+
+audio.volume = 0.5;
+
 function handleListAnimation() {
+  const mainList = document.querySelector('.list__main');
   const listBtn = document.querySelector('.menu');
   const list = document.querySelector('.list');
-  const mainList = document.querySelector('.list__main');
   const overlay = document.querySelector('.list__overlay');
   const closeBtn = document.querySelector('.list__header i');
 
@@ -21,24 +33,84 @@ function handleListAnimation() {
   };
 }
 
-function handleVolumeButton() {
-
-}
-
-function handleVolumeButton() {
-  const playBtn = document.querySelector('.play');
-  console.log(playBtn);
+function formatDuration(sec) {
+  sec = Math.round(sec);
+  let min = Math.floor(sec / 60);
+  sec -= min * 60;
+  sec = sec.toString().padStart(2, '0');
+  return `${min}:${sec}`;
 }
 
 function renderSongs() {
+  songs.forEach((song, index) => {
+    const songElement = document.createElement('div');
+    songElement.className = `song song${index}`;
+    songElement.innerHTML = `
+      <div class="song__img">
+        <div class="song__play">
+          <i class="fa-solid fa-circle-play"></i>
+        </div>
+        <img src="${song.image}" alt="">
+      </div>
+      <div class="song__detail">
+        <p class="song__author">${song.author}</p>
+        <p class="song__title">${song.name}</p>
+      </div>
+      <div class="song__duration"></div>
+    `;
+    listContent.appendChild(songElement);
+
+    let audio = document.createElement('audio');
+    audio.src = song.path;
+    audio.addEventListener('loadeddata', () => {
+      songElement.querySelector('.song__duration').innerHTML = formatDuration(audio.duration);
+    });
+  });
+}
+
+function handlePlayButton() {
+  const playBtn = document.querySelector('.play');
+  playBtn.onclick = () => {
+    if (audio.paused) {
+      playBtn.innerHTML = '<i class="fa-fw fa-solid fa-circle-pause"></i>';
+      audio.play();
+    } else {
+      playBtn.innerHTML = '<i class="fa-fw fa-solid fa-circle-play"></i>';
+      audio.pause();
+    }
+  };
+}
+
+function loadAudio() {
+  document.querySelector(`.song${oldIndex}`).classList.remove('song--playing');
+  const currentSong = document.querySelector(`.song${currentIndex}`);
+  currentSong.classList.add('song--playing');
+  currentTime.innerHTML = '0:00';
+  audio.src = songs[currentIndex].path;
+  audio.addEventListener('loadeddata', () => {
+    duration.innerHTML = formatDuration(audio.duration);
+  });
+}
+
+function handleVolumeButton() {
+  const volumePanel = document.querySelector('.volume__panel');
+  const volumeFill = document.querySelector('.volume__fill');
   
+  volumePanel.addEventListener('mouseup', (e) => {
+    volumePanel.onmousedown = () => {
+      volumePanel.onmousemove = (e) => {
+        console.log(e);
+      }
+    }
+  });
 }
 
 function start() {
   renderSongs();
   handleListAnimation();
-  handleVolumeButton();
+  loadAudio();
   handlePlayButton();
+  handleVolumeButton();
 }
 
 start();
--- assets/javascript/songs.js
+++ assets/javascript/songs.js
@@ -2,49 +2,51 @@ const songs = [
   {
     name: 'Myself',
     author: 'NAV',
-    path: '../musics/song1.mp3',
-    image: '../images/img1.png'
+    path: './assets/musics/song1.mp3',
+    image: './assets/images/img1.png'
   },
   {
     name: 'DARK BEACH',
     author: 'Pastel Ghost',
-    path: '../musics/song2.mp3',
-    image: '../images/img2.png'
+    path: './assets/musics/song2.mp3',
+    image: './assets/images/img2.png'
   },
   {
     name: 'CLOUDS',
     author: 'Pastel Ghost',
-    path: '../musics/song3.mp3',
-    image: '../images/img3.png'
+    path: './assets/musics/song3.mp3',
+    image: './assets/images/img3.png'
   },
   {
     name: 'letting go',
     author: 'potsu',
-    path: '../musics/song4.mp3',
-    image: '../images/img4.png'
+    path: './assets/musics/song4.mp3',
+    image: './assets/images/img4.png'
   },
   {
     name: 'Lexapro Delirium',
     author: 'Sewerslvt',
-    path: '../musics/song5.mp3',
-    image: '../images/img5.png'
+    path: './assets/musics/song5.mp3',
+    image: './assets/images/img5.png'
   },
   {
     name: 'MAKE-ME-SAD',
     author: 'Sewerslvt',
-    path: '../musics/song6.mp3',
-    image: '../images/img6.png'
+    path: './assets/musics/song6.mp3',
+    image: './assets/images/img6.png'
   },
   {
     name: 'tokyoghost777',
     author: 'metadata',
-    path: '../musics/song7.mp3',
-    image: '../images/img7.png'
+    path: './assets/musics/song7.mp3',
+    image: './assets/images/img7.png'
   },
   {
     name: 'ON ME',
     author: 'Cochise',
-    path: '../musics/song8.mp3',
-    image: '../images/img8.png'
+    path: './assets/musics/song8.mp3',
+    image: './assets/images/img8.png'
   }
 ];
+
+export default songs;
